613fa70a63dd7c5c493f28ba107b144d181d7def
Zach Drummond <zachdrummond3@gmail.com>
2020-11-03 20:31:23 -0500

    Imported react-router-dom and created about, portfolio, and contact files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/" component={About}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/portfolio" component={Portfolio}/>
+          <Route exact path="/contact" component={Contact}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
