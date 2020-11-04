6da8ceb00df97bf5724daaf9bdc5b87f8498e1a4
Zach Drummond <zachdrummond3@gmail.com>
2020-11-03 21:14:09 -0500

    Added styling to navbar

--- src/components/NavBar/Name.jsx
+++ src/components/NavBar/Name.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-const Name = () => {
+const Name = () => { 
   return (
     <>
       {/* < Arrow */}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
+import "./NavBar.css"
 import Name from "./Name";
+import Links from "./Links";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Name/>
+      <Links/>
     </nav>
   );
 };
