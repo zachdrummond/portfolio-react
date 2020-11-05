550406652488d973f486de6a1dcf3478295a39f9
Zach Drummond <zachdrummond3@gmail.com>
2020-11-05 10:00:43 -0500

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
       <>
         <NavBar />
         <Switch>
-          <Route exact path="/about" component={About} />
-          <Route exact path="/portfolio-react" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio-react/" component={About} />
+          <Route exact path="/portfolio-react/about" component={About} />
+          <Route exact path="/portfolio-react/portfolio" component={Portfolio} />
+          <Route exact path="/portfolio-react/contact" component={Contact} />
           <Route path="/" component={About} />
         </Switch>
         <Footer />
