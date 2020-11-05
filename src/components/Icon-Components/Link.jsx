ff174ff2a16b3a4a0ddb0032d61f8a3f9d870235
Zach Drummond <zachdrummond3@gmail.com>
2020-11-04 19:58:48 -0500

    Fixed navbar

--- src/components/Icon-Components/Link.jsx
+++ src/components/Icon-Components/Link.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Link = (props) => {
   return (
-    <a href={props.href}>
+    <a href={props.href} className="mr-1">
       <svg
         width="2em"
         height="2em"
--- src/components/NavBar/Name.jsx
+++ src/components/NavBar/Name.jsx
@@ -1,38 +1,38 @@
 const Name = () => {
   return (
     <>
-      {/* < Arrow */}
-      <svg
-        width="2em"
-        height="2em"
-        viewBox="0 0 16 16"
-        className="bi bi-chevron-left"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-        />
-      </svg>
-      {/* Zach Drummond */}
       <p id="name" className="navbar-brand font-weight-bold font m-0">
+        {/* < Arrow */}
+        <svg
+          width="1.5em"
+          height="1.5em"
+          viewBox="0 0 16 16"
+          className="bi bi-chevron-left"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+          />
+        </svg>
         Zach Drummond
+        {/* > Arrow */}
+        <svg
+          width="1.5em"
+          height="1.5em"
+          viewBox="0 0 16 16"
+          className="bi bi-chevron-right"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
       </p>
-      {/* > Arrow */}
-      <svg
-        width="2em"
-        height="2em"
-        viewBox="0 0 16 16"
-        className="bi bi-chevron-right"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-        />
-      </svg>
+      {/* Navbar Collapsible Button */}
       <button
         className="navbar-toggler"
         type="button"
